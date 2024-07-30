095a2b0b39cab9b081ab21657d0d851b6863e750
SzymonSurowka <szymon121122@gmail.com>
2024-07-30 21:15:31 +0200

    feat: project update

--- Project 5/script.js
+++ Project 5/script.js
@@ -1,16 +1,19 @@
 const input = document.getElementById('pass');
 const div = document.querySelector(".message");
-const password = "user";
-const message = "wyjechałam na zawsze";
+const passwords = ["user", "wiosna", "Ania"];
+const messages = ["wyjechałam na zawsze", "piękna pora roku", "Fajny styl masz"];
 
 input.addEventListener('input', (e) => {
-    // console.log(e.target.value);
-    if(password === e.target.value) {
-    div.textContent = message;
-    e.target.value = '';
-    } else {
-        div.textContent = '';
+    div.textContent = '';
+const text = e.target.value;
+// console.log(text);
+passwords.forEach((password, index) => {
+    if(password === text) {
+        console.log(password, index);
+        div.textContent = messages[index];
+        e.target.value = '';
     }
+})
 })
 
 input.addEventListener('focus', (e) => {
@@ -20,3 +23,10 @@ e.target.classList.add('active');
 input.addEventListener('blur', (e) => {
 e.target.classList.remove('active');
 })
+
+    // if(password === e.target.value) {
+    // div.textContent = message;
+    // e.target.value = '';
+    // } else {
+    //     div.textContent = '';
+    // }
